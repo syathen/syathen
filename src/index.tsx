42a94dd5f4db54300ba2336c2d5bd6ca4d0b3259
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2023-04-05 17:50:34 -0400

    Housekeeping tasks
    Remove reportWebVitals
    Add 404
    Cleanup template text

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
-/**
- * index.tsx
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
@@ -23,8 +16,6 @@ import { HelmetProvider } from 'react-helmet-async';
 import { configureAppStore } from 'store/configureStore';
 
 import { ThemeProvider } from 'styles/theme/ThemeProvider';
-
-import reportWebVitals from 'reportWebVitals';
 
 // Initialize languages
 import './locales/i18n';
@@ -61,8 +52,3 @@ if (module.hot) {
     // No need to render the App again because i18next works with the hooks
   });
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
